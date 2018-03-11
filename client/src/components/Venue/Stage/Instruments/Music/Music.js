b85c2e890ec42a587843cffe940df125a4cca319
esharri2 <esharri2@gmail.com>
2018-03-11 12:48:31 -0400

    start performance functionality

--- client/src/components/Venue/Stage/Instruments/Music/Music.js
+++ client/src/components/Venue/Stage/Instruments/Music/Music.js
@@ -1,54 +1,47 @@
 import React, { Component } from 'react';
 import Square from './Square';
 
-class Music extends Component {
-
-    //NOTE the math that happens on x and y might could happen on sound updaters func def (pass in x and y as params)
-
-    //NO
-    //pull in updaters
-    //set state to default effects values
-    //run all the updaters with state
-
-    //YES
-    //or push specific to each square and let it update itself!!!! this sounds good, e.g. here's your method, here's your values
-    //each square will have it's own state of "active" or not
+class Music extends Component {    
     render() {
         const soundUpdaters = this.props.soundUpdaters;
-        //TODO create these Square components with a loop over the updaters (i.e. one square per updater)
         return (
             <div className="music">
                 <Square
                     name="A"
+                    isPerformer={this.props.isPerformer}
                     instrumentProps={this.props}
                     soundUpdater={soundUpdaters.jcReverb}
                 />
                 <Square
                     name="B"
+                    isPerformer={this.props.isPerformer}                   
                     instrumentProps={this.props}
                     soundUpdater={soundUpdaters.distortion}
                 />
                 <Square
                     name="C"
+                    isPerformer={this.props.isPerformer}                    
                     instrumentProps={this.props}
                     soundUpdater={soundUpdaters.pingPongDelay}
                 />
                 <Square
                     name="D"
+                    isPerformer={this.props.isPerformer}                   
                     instrumentProps={this.props}
                     soundUpdater={soundUpdaters.tremolo}
                 />
                 <Square
                     name="E"
+                    isPerformer={this.props.isPerformer}                    
                     instrumentProps={this.props}
                     soundUpdater={soundUpdaters.pitchShift}
                 />
                 <Square
                     name="F"
+                    isPerformer={this.props.isPerformer}                    
                     instrumentProps={this.props}
                     soundUpdater={soundUpdaters.phaser}
                 />
-
             </div>
         );
     }
