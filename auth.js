0b333827a67e7f05292960dd9700228d4fbf1884
Charlie Bowden <charles.bowden@gmail.com>
2018-03-17 11:08:27 -0400

    Updates to Controllers and auth.js

--- auth.js
+++ auth.js
@@ -1,4 +1,6 @@
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const User = require('./models/user');
+
 module.exports = (passport) => {
     passport.serializeUser((user, done) => {
         done(null, user);
@@ -12,10 +14,23 @@ module.exports = (passport) => {
         callbackURL: "http://localhost:3001/auth/google/callback"
         },
         (token, refreshToken, profile, done) => {
-            console.log(profile);
-            return done(null, {
-                profile: profile,
-                token: token
+            // check if user already exists in our own db
+            User.findById({ id: profile.id }).then((currentUser) => {
+                if (currentUser) {
+                    // already have this user
+                    console.log('user is: ', currentUser);
+                    done(null, currentUser);
+                } else {
+                    // if not, create user in our db
+                    new User({
+                        id: profile.id,
+                        name: profile.displayName
+                    }).save().then((newUser) => {
+                        console.log('created new user: ', newUser);
+                        done(null, newUser);
+                    });
+                }
             });
-        }));
+        })
+    );
 };
--- controllers/Controller.js
+++ controllers/Controller.js
@@ -32,13 +32,13 @@ module.exports = {
   },
   update: function(req, res) {
     db.User
-      .findOneAndUpdate({ _id: req.params.id }, req.body)
+      .findOneAndUpdate({ id: req.params.id }, req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   remove: function(req, res) {
     db.User
-      .findById({ _id: req.params.id })
+      .findById({ id: req.params.id })
       .then(dbModel => dbModel.remove())
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
