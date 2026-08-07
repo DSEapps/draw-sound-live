15849061b74ba11d986cdbd9ee3d3bbd1177256a
Scott Bower <scottbowerwerk@gmail.com>
2026-08-07 16:17:07 -0400

    Updated authentication process

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,9 +1,40 @@
 import React, { Component } from "react";
 import API from "../../utils/API";
-import { GoogleLogin } from "react-google-login";
 import { withRouter } from "react-router-dom";
 
+// Decodes the payload of a Google ID token (JWT) using only built-in
+// browser functions - no library needed, and no chance of a modern
+// npm package tripping up this old build toolchain.
+function decodeJwtPayload(token) {
+  const base64Url = token.split(".")[1];
+  const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+  const jsonPayload = decodeURIComponent(
+    atob(base64)
+      .split("")
+      .map(c => "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2))
+      .join("")
+  );
+  return JSON.parse(jsonPayload);
+}
+
 class Login extends Component {
+
+  componentDidMount() {
+    const script = document.createElement("script");
+    script.src = "https://accounts.google.com/gsi/client";
+    script.async = true;
+    script.onload = () => {
+      window.google.accounts.id.initialize({
+        client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID,
+        callback: this.handleSuccess
+      });
+      window.google.accounts.id.renderButton(
+        document.getElementById("google-login-button"),
+        { theme: "outline", size: "large", text: "continue_with" }
+      );
+    };
+    document.body.appendChild(script);
+  }
 
   componentWillUpdate(nextProps, nextState) {
     if (nextProps.userInfo.id && !this.props.userInfo.id) {
@@ -12,15 +43,12 @@ class Login extends Component {
   }
 
   handleSuccess = (response) => {
-    const google_id = response.profileObj.googleId;
-    const name = response.profileObj.givenName;
+    const decoded = decodeJwtPayload(response.credential);
+    const google_id = decoded.sub;
+    const name = decoded.given_name;
     API.findOrCreateUser({ id: google_id, name: name }).then(userData => {
       this.props.handleUserData(userData);
     }).catch(err => console.log(err))
-  }
-
-  handleFailure = () => {
-    console.log("fail.")
   }
 
   cancelClick=()=>{
@@ -36,13 +64,7 @@ class Login extends Component {
           <p>To continue to the performace space you will need to login with your Google account.</p>
           <div className="actions">
             <button onClick={this.cancelClick}>Cancel</button>
-            <GoogleLogin
-              clientId="265275680499-5g10j3l9srs97m57iffmn0riqk9p5evf.apps.googleusercontent.com"
-              buttonText="Continue"
-              onSuccess={this.handleSuccess}
-              onFailure={this.handleFailure}
-              className="button-primary"
-            />
+            <div id="google-login-button"></div>
           </div>
         </div>
       </div>
@@ -52,6 +74,3 @@ class Login extends Component {
 }
 
 export default withRouter(Login);
-
-
-
